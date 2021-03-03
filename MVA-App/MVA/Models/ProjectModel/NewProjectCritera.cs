00298669e51350e25031a0316c1e586421f2cac0
Derek Cacciotti <applegamer247@gmail.com>
2021-03-03 07:58:12 -0500

    added validation and model classes

---
+++ MVA-App/MVA/Models/ProjectModel/NewProjectCritera.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace MVA.Models.ProjectModel
+{
+    public class NewProjectCritera
+    {
+        public NewProjectInit ProojectData { get; set; }
+        public int numofParticpants { get; set; }
+        public int numofBaselinePoints { get; set; }
+        public int numofinterventionPoints { get; set; }
+        public int numOfMinStaggerPoints { get; set; }
+        public int? outlierFK { get; set; }
+
+    }
+}
--- MVA-App/MVA/Views/NewCriteraPage.xaml.cs
+++ MVA-App/MVA/Views/NewCriteraPage.xaml.cs
@@ -29,6 +29,44 @@ namespace MVA
 
         void btncriteraNext_Clicked(System.Object sender, System.EventArgs e)
         {
+            var outlier = pickerOutlier.SelectedItem as Outlier;
+
+            if(string.IsNullOrEmpty(txtNumofParticpants.Text) || txtNumofParticpants.Text == "0")
+            {
+                DisplayAlert("Error", "Number of Particpants is required", "OK");
+            }
+            else if(string.IsNullOrEmpty(txtNumofBaselinepoints.Text) || txtNumofBaselinepoints.Text == "0")
+            {
+                DisplayAlert("Error", "Number of baseline points is required", "OK");
+            }
+            else if(string.IsNullOrEmpty(txtnumOfInterventionPoints.Text) || txtnumOfInterventionPoints.Text == "0")
+            {
+                DisplayAlert("Error", "Number of intervention points is required", "OK");
+            }
+            else if(string.IsNullOrEmpty(txtnumofmininiumStaggerPoints.Text) || txtnumofmininiumStaggerPoints.Text == "0")
+            {
+                DisplayAlert("Error", "Number of Mininium Stagger Points is required", "OK");
+            }
+            else
+            {
+                NewProjectCritera projectCritera = new NewProjectCritera();
+                projectCritera.ProojectData = _initdata;
+                projectCritera.numofBaselinePoints = Convert.ToInt32(txtNumofParticpants.Text);
+                projectCritera.numofinterventionPoints = Convert.ToInt32(txtnumOfInterventionPoints.Text);
+                projectCritera.numOfMinStaggerPoints = Convert.ToInt32(txtnumofmininiumStaggerPoints.Text);
+
+                if(outlier.codeOutlierPk == 0)
+                {
+                    projectCritera.outlierFK = null;
+                }
+                else
+                {
+                    projectCritera.outlierFK = outlier.codeOutlierPk;
+                }
+
+                // Navigate to particpants screem
+
+            }
         }
     }
 }
